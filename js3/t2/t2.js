62074e15409a82b39e91ac4179d1bc54f60177c7
darri <iaroslag@metropolia.fi>
2023-09-01 15:52:04 +0300

    tehtävä 2 tehty

--- js3/t2/t2.js
+++ js3/t2/t2.js
@@ -28,3 +28,31 @@ const todoList = [
 ];
 
 // add your code here
+const ul = document.querySelector('ul');
+
+for (const todo of todoList) {
+    const li = document.createElement('li');
+    const input = document.createElement('input');
+    const label = document.createElement('label');
+    const button = document.createElement('button')
+
+    input.type = 'checkbox';
+    input.id = 'todo-' + todo.id;
+    input.checked = todo.completed;
+
+    input.addEventListener('change', function(evt){
+      todo.completed = input.checked;
+      console.log(todoList);
+
+    });
+
+    label.htmlFor = 'todo-' + todo.id;
+    label.innerText = todo.task;
+
+    button.innerHTML = '&#x2A09';
+
+    li.appendChild(input);
+    li.appendChild(label);
+    li.appendChild(button);
+    ul.appendChild(li)
+}
