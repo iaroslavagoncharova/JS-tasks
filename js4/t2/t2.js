59538a3e832b6f23066ae5370f8958d14fc45289
darri <iaroslag@metropolia.fi>
2023-09-01 19:19:21 +0300

    tehtävä 2 tehty

--- js4/t2/t2.js
+++ js4/t2/t2.js
@@ -771,3 +771,75 @@ const restaurants = [
 ];
 
 // your code here
+
+restaurants.sort(function(a, b) {
+if (a.name < b.name) {
+      return -1;
+  }
+  if (a.name > b.name) {
+      return 1;
+  }
+  return 0;
+});
+
+const table = document.querySelector('table');
+
+for (const restaurant of restaurants) {
+  const tr = document.createElement('tr');
+  const td1 = document.createElement('td');
+  td1.innerHTML = restaurant.name;
+  const td2 = document.createElement('td');
+  td2.innerHTML = restaurant.address;
+
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+
+  table.appendChild(tr);
+
+  tr.addEventListener('click', function() {
+    const all = document.querySelectorAll('tr');
+      for (let i = 0; i < all.length; i++) {
+          all[i].classList.remove('highlight');
+      }
+
+      tr.classList.add('highlight');
+
+      showDetails(restaurant);
+  });
+}
+
+
+function showDetails(restaurant) {
+
+  const dialog = document.querySelector('dialog');
+    dialog.innerHTML = '';
+
+    const name = document.createElement('p');
+    name.innerHTML = `Name: ${restaurant.name}`;
+
+    const address = document.createElement('p');
+    address.innerHTML = `Address: ${restaurant.address}`;
+
+    const postalCode = document.createElement('p');
+    postalCode.innerHTML = `Postal Code: ${restaurant.postalCode}`;
+
+    const city = document.createElement('p');
+    city.innerHTML = `City: ${restaurant.city}`;
+
+    const phoneNumber = document.createElement('p');
+    phoneNumber.innerHTML = `Phone Number: ${restaurant.phone}`;
+
+    const company = document.createElement('p');
+    company.innerHTML = `Company: ${restaurant.company}`;
+
+    dialog.appendChild(name);
+    dialog.appendChild(address);
+    dialog.appendChild(postalCode);
+    dialog.appendChild(city);
+    dialog.appendChild(phoneNumber);
+    dialog.appendChild(company);
+
+    dialog.showModal();
+}
+
+
